7d5e8dbeaec77d34b0f957e184b1e0bc3aa93298
yanazelieva <alunettt@gmail.com>
2023-05-18 14:42:11 -0400

    Divide without operator solution

--- src/week01/DivideWithoutOperator.java
+++ src/week01/DivideWithoutOperator.java
@@ -12,8 +12,37 @@ public class DivideWithoutOperator {
 
     public static void main(String[] args) {
 
-//solution
+        //System.out.println(divide(4,3));
+
     }
+
+
+    public static int divide(double num1, double num2){
+
+        if(num2 == 0){
+            throw new ArithmeticException("Divisor can not be zero.");
+        }
+
+        int result = 0;
+
+        while (num1>=num2){
+            num1 -= num2;
+            result++;
+
+        }
+
+        return result;
+
+    }
+
+
+
+
+
+
+
+
+
 
 }
 
