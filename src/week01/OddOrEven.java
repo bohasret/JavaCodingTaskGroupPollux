68b22aa6ab6706ed8f087afb2b25f84c684207d0
yanazelieva <alunettt@gmail.com>
2023-05-18 14:14:46 -0400

    odd or even solution

--- src/week01/OddOrEven.java
+++ src/week01/OddOrEven.java
@@ -17,11 +17,19 @@ public class OddOrEven {
 
     public static void main(String[] args) {
 
-        //solution
 
 
 
 
+
+    }
+
+    public static void identify(int number){
+        if(number % 2 == 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
     }
 
 
