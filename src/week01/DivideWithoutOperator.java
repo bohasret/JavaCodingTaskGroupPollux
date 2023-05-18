ecbac493f12b31376edfd33ccdb9af8a2a0357ae
hasretbolatasni <hasretbolat5@gmail.com>
2023-05-18 12:08:18 -0400

    Created first week and .gitignore

---
+++ src/week01/DivideWithoutOperator.java
@@ -0,0 +1,19 @@
+package week01;
+
+public class DivideWithoutOperator {
+
+    /*
+
+     Numbers - Divide without / operator:
+     Write a method that can divide two numbers without using division operator
+
+     */
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
