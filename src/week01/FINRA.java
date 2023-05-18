a488b95cbaf418e990c0f118af08ffe3fba618dc
yanazelieva <alunettt@gmail.com>
2023-05-18 14:24:14 -0400

    FINRA solution

--- src/week01/FINRA.java
+++ src/week01/FINRA.java
@@ -10,6 +10,42 @@ public class FINRA {
      */
 
     public static void main(String[] args) {
-//solution
+
+        //System.out.println(finra());
     }
+
+
+    public static String finra(){
+
+        String result = "";
+
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 15 == 0) {
+                result += "FINRA ";
+
+            } else if (i % 5 == 0) {
+                result += "RA ";
+
+            } else if(i % 3 == 0){
+                result += "FIN ";
+
+            } else{
+                result += i +" ";
+            }
+
+        }
+       return result;
+
+
+
 }
+
+
+
+
+
+}
+
+
+
