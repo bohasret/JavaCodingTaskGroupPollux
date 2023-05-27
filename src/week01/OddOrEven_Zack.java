2665d2ffe03ca803c94cecae513ef32de0ab417c
zack <mziyag5187@gmail.com>
2023-05-27 10:57:46 +0300

    small update on OddOrEven

--- src/week01/OddOrEven_Zack.java
+++ src/week01/OddOrEven_Zack.java
@@ -2,7 +2,7 @@ package week01;
 
 public class OddOrEven_Zack {
     /*
-     Numbers-  Odd or even:
+     Numbers - Odd or even:
      Write  a method which can identify given number is even or odd
      Output ex:
      identify(5); ->"Odd"
