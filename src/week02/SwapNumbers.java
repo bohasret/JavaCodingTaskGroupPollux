e752a1314393261b2a0551865c30b7df4d47af1d
iremaktass <aktasirem3495@gmail.com>
2023-05-31 22:17:50 -0400

    updated Swap numbers

--- src/week02/SwapNumbers.java
+++ src/week02/SwapNumbers.java
@@ -1,5 +1,31 @@
 package week02;
 
 public class SwapNumbers {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 200;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+
+
+
+
+
+    }
+
+
+
 
 }
+// Swap two variable values without using a third variable
