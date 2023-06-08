5f84a50abb280151303a022d85cec36f12215389
yana1zelieva <yzelieva@gmail.com>
2023-06-08 11:45:18 -0400

    tasks week 3

---
+++ src/week03/Task1/Yana.java
@@ -0,0 +1,30 @@
+package week03.Task1;
+
+public class Yana {
+
+    //Write a method that can check if a number is prime or not
+    public static void main(String[] args) {
+        System.out.println(isPrime(3));
+        System.out.println(isPrime(17));
+    }
+
+
+
+    public static boolean isPrime(int num){
+
+        if(num<=1){ //1 and 0 are not prime numbers
+            return false;
+        }
+
+
+        for(int i = 2; i <= num; i++){  //There is no number that can be completely divided by more than half of the number itself.
+                                        // We need to loop through two to the number itself divided by two (num/2).
+            if((num%i)==0)
+                return  false;
+        }
+
+
+        return true;
+
+    }
+}
---
+++ src/week03/Task2/Yana.java
@@ -0,0 +1,55 @@
+package week03.Task2;
+
+import java.util.Collections;
+
+public class Yana {
+
+    //Reverse Negative Number
+
+    public static void main(String[] args) {
+        reverse(-345);
+        reverse2(-2231);
+    }
+
+
+
+    //with string
+    public static void reverse(int num){
+        num = Math.abs(num);
+
+        String number = num + "";
+        String reversed = "";
+
+        for (int i = number.length()-1; i >= 0; i--) {
+            reversed += number.charAt(i);
+        }
+
+        num = Integer.parseInt(reversed);
+        num *= -1;
+
+        System.out.println("Reversed: " + num);
+    }
+
+
+
+    //mathematical logic
+    public static void reverse2(int number){
+        int reverse = 0;
+        number = Math.abs(number);
+
+        while(number != 0) {
+
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+
+        }
+        reverse *= -1;
+
+
+        System.out.println("Reversed: " + reverse);
+    }
+
+
+
+}
