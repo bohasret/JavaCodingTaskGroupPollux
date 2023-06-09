8a2ad57b2c5c443b3556c5c11e24c5c8db30453f
iremaktass <aktasirem3495@gmail.com>
2023-06-09 14:43:26 -0400

    created

---
+++ src/week03/Task2/Irem.java
@@ -0,0 +1,14 @@
+package week03.Task2;
+
+public class Irem {
+
+    /**
+     * reverse digits of a negative number and return it as int
+     */
+}
+/*
+
+2️⃣ Numbers -- Reverse Negative Number
+Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+ */
