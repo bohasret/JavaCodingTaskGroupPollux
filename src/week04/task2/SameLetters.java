5fafca2468bf23d32c35c8ef258e69a257020c9f
Viktoriya Cydeo <vikochka3@yahoo.ca>
2023-06-14 19:11:19 -0400

    finished frequency same letters task

---
+++ src/week04/task2/SameLetters.java
@@ -0,0 +1,32 @@
+package week04.task2;
+
+import java.util.Arrays;
+
+public class SameLetters {
+    //Write a return method that checks if a string is built out of the same letters as another string.
+    //  Ex: same("abc", "cab"); ==> true
+    //      same("abc", "abb"); ==> false
+
+    public static boolean sameLetters( String a, String b){
+        char[] ch1 = a.toCharArray();
+        char[] ch2 = b.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        String a1 = "", a2 = "";
+
+        for (char each: ch1){
+            a1 += each;
+        }
+
+        for(char each : ch2){
+            a2+=each;
+        }
+        return a1.equals(a2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sameLetters("abc", "cab"));
+        System.out.println(sameLetters("abc", "abb"));
+    }
+}
