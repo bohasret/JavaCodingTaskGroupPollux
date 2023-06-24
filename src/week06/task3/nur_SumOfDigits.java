632f9b5367f244451fb0b41e64ac516e117828aa
nur_Cydeo <nur@cydeo.com>
2023-06-24 01:20:10 -0400

    Week6 Updates

---
+++ src/week06/task3/nur_SumOfDigits.java
@@ -0,0 +1,25 @@
+package week06.task3;
+
+import java.util.Collections;
+
+public class nur_SumOfDigits {
+    public static void main(String[] args) {
+        String str= "12 java 5 apple 3";
+
+
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch= str.charAt(i);
+            if(Character.isDigit(ch)){
+
+            }
+
+        }
+
+    }
+
+}
+/*
+Write a method that can return the sum of the digits in a String
+Ex:  "12 java 5 apple 3"  ==>  20
+ */
