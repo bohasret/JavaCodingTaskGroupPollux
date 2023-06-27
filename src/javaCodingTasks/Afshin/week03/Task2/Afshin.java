8480ed3cf9b095163f3534010c33732d005dcb61
Afshin_cydeo <afshinkiyaninasab@gmail.com>
2023-06-27 15:36:15 -0400

    fixed titles and stuff

---
+++ src/javaCodingTasks/Afshin/week03/Task2/Afshin.java
@@ -0,0 +1,9 @@
+package javaCodingTasks.Afshin.week03.Task2;
+
+public class Afshin {
+}
+/*
+ Numbers -- Reverse Negative Number
+Write a return method that can reverse digits of a negative number and return it as int
+input: -35  output: -53
+ */
