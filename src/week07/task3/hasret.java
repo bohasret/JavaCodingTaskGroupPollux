2df6dafc0cfabe7a4db8921d4af1fe78a2f2a76a
Zack <62723491+mziyag5187@users.noreply.github.com>
2023-07-12 22:02:47 +0300

    Update hasret.java

--- src/week07/task3/hasret.java
+++ src/week07/task3/hasret.java
@@ -3,10 +3,7 @@ package week07.task3;
 import java.util.Arrays;
 
 public class hasret {
-   //Bubble sort
-    //(The logic we use for sorting an integer array ascending and descending order)
-
-    //Array_SortDescending
+     //Array_SortDescending
     //Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
     //Ex: int[] arr = {10,20,7, 8, 90};
     //arr = sortDecending(arr); ==> {90, 20, 10, 8, 7};
